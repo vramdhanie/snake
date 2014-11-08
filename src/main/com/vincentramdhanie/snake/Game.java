fc3b07debd10b2f6203d5e39c3461853da382b28
unknown <Student@CF-LAB10-LECT.Lab.SBCS.Edu>
2014-11-08 15:02:26 -0400

    Added animation basics

--- src/main/com/vincentramdhanie/snake/Game.java
+++ src/main/com/vincentramdhanie/snake/Game.java
@@ -9,7 +9,36 @@ import javax.swing.JFrame;
  * @author vramdhanie@gmail.com (Vincent Ramdhanie)
  */
 public class Game {
+
+	static java.util.Map<Integer, Long> f;
+
 	public static void main(String args[]){
 		JFrame f = new SnakeFrame();
+		//run(1, 200);
 	}
+
+	public static void run(int first, int last){
+		long start = 0;
+		long fin = 0;
+		for(int i = first; i <= last; i++){
+			f = new java.util.HashMap<Integer, Long>();
+			f.put(0, 0L);
+			f.put(1, 1L);
+			start = System.nanoTime();
+			System.out.printf("fib(%d) = %d\t", i, fib(i));
+			fin = System.nanoTime();
+			System.out.printf("Took %d nanoseconds\n", fin-start);	
+		}
+	}
+
+	public static long fib(int n){
+		if(f.containsKey(n)){
+			return f.get(n);
+		}else{
+			long ans = fib(n-1) + fib(n - 2);
+			f.put(n, ans);
+			return ans;
+		}
+	}
+
 }
--- src/main/com/vincentramdhanie/snake/Segment.java
+++ src/main/com/vincentramdhanie/snake/Segment.java
@@ -1,10 +1,57 @@
 package com.vincentramdhanie.snake;
 
+import java.awt.Shape;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.NoninvertibleTransformException;
+import java.util.List;
+import java.util.ArrayList;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
 /**
  * A segment of a snake. A collection of Segments
  * make up the body of the snake.
  * @author vramdhanie@gmail.com (Vincent Ramdhanie)
  */
 public class Segment {
-	
+	Shape shape;
+	List<AffineTransform> transforms;
+
+	public Segment(){
+		shape = new Rectangle2D.Double(500, 100, 10, 3);
+		transforms = new ArrayList<AffineTransform>();
+	}
+
+	public void addTransform(AffineTransform af){
+		transforms.add(af);
+	}
+
+	public void move(){
+		
+	}
+
+	public void draw(Graphics2D g){
+		g.setColor(Color.GREEN);
+		AffineTransform af = new AffineTransform();
+		if(!transforms.isEmpty()){
+			//af.concatenate(transforms.get(0));
+			shape = transforms.get(0).createTransformedShape(shape);
+			transforms.remove(0);
+		}
+		//g.transform(af);
+		g.fill(shape);
+		//try{
+		//	g.transform(af.createInverse());
+		//}catch(NoninvertibleTransformException e){
+
+		//}
+		
+	}
+
+	public Shape getShape(){
+		return shape;
+	}
+
+
 }
