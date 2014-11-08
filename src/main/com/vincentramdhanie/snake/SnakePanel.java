39c96c2bafea80e0f56d4e5189f1f90b11da2083
unknown <Student@CF-LAB10-LECT.Lab.SBCS.Edu>
2014-11-08 10:27:52 -0400

    Added override for paintComponent

--- src/main/com/vincentramdhanie/snake/SnakePanel.java
+++ src/main/com/vincentramdhanie/snake/SnakePanel.java
@@ -2,10 +2,31 @@ package com.vincentramdhanie.snake;
 
 import javax.swing.JPanel;
 import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Ellipse2D;
 
 public class SnakePanel extends JPanel{
+
+	Rectangle2D rect;
+	Ellipse2D circle;
+
 	public SnakePanel(){
 		super();
 		setBackground(Color.BLACK);
+		rect = new Rectangle2D.Double(20, 20, 300, 150);
+		circle = new Ellipse2D.Double(110, 35, 120, 120); 
+	}
+
+	@Override
+	protected void paintComponent(Graphics g){		
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+
+		g.setColor(Color.WHITE);
+		g2.fill(rect);
+		g.setColor(Color.RED);
+		g2.fill(circle);
 	}
 }
